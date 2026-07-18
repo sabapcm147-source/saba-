12bf2610007222010fbed89cbd6d7666a6fe8f11
sabapcm147-source <sabapcm147@gmail.com>
2026-07-18 12:50:00 +0600

    Create even_odd.c

---
+++ even_odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    //  C code to identify even or odd number
+    int a;
+    printf("Take input a:");
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("\n the number is even ");
+    }
+    else{
+        printf("\n the number is odd ");
+    }
+
+    return 0;
+}
